0dae12f430fc6851c9df84991d318ca0cc237821
Alex <alex@alexnord.org>
2024-05-08 21:12:03 -0600

    Added basic output metadata collection

--- TestSplash.pl
+++ TestSplash.pl
@@ -8,6 +8,8 @@ sub Max { my $A = shift; my $B = shift; return $A if ($A>$B); return $B; }
 sub Min { my $A = shift; my $B = shift; return $A if ($A<$B); return $B; }
 
 
+sub AggregateAllResults;
+sub CompileBasicResults;
 sub FamilySplash;
 sub BigBadSplash;
 sub DetermineTargetSeq;
@@ -63,13 +65,285 @@ elsif ($OPTIONS{'single-seq'}) { die "\n  Not yet supported\n\n";         }
 else {   die "\n  ERROR:  Failed to recognize protein input\n\n";         }
 
 
-
 1;
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+###########################################################################
+#
+#  Subroutine: AggregateAllResults
+#
+sub AggregateAllResults
+{
+	my $all_fams_dir_name = shift;
+
+	$all_fams_dir_name =~ /^(\S+\/)[^\/]+\/$/;
+	my $final_results_file_name = $1.'Splash-Summary.out'; # Should be same as 'output-dir', but where's the fun?
+
+
+	# We'll want to capture the splicing dinucleotides
+	my %ThreePrime;
+	my %ThreePrimeOne;
+	my %ThreePrimeTwo;
+	my %FivePrime;
+	my %FivePrimeOne;
+	my %FivePrimeTwo;
+
+	# How many sequences had some splicing action?
+	# How many covered the full length of the model?
+	# How many (of each of the above) incorporated 'missed' exons?
+	my $total_num_spliced  = 0;
+	my $num_full_model     = 0;
+	my $inputs_with_missed = 0;
+	my $full_with_missed   = 0;
+
+
+	opendir(my $AllFamsDir,$all_fams_dir_name)
+		|| die "\n  ERROR:  Failed to open all-family meta-directory '$all_fams_dir_name'\n\n";
+	while (my $family = readdir($AllFamsDir)) 
+	{
+		
+		next if ($family =~ /^\./);
+		$family =~ s/\/$//;
+
+		my $family_dir_name   = $all_fams_dir_name.$family.'/';
+		my $summary_file_name = $family_dir_name.'summary.out';
+
+		next if (!(-e $summary_file_name));
+
+
+		# Looks like we're on!
+		open(my $SummaryFile,'<',$summary_file_name)
+			|| die "\n  ERROR:  Failed to open summary file '$summary_file_name' for family '$family'\n\n";
+
+
+		# First off, grab the metadata that will let us navigate
+		# this summary file smoothtly
+		my $num_fam_inputs;
+		while (my $line = <$SummaryFile>)
+		{
+			if ($line =~ /NUM SPLASHED : (\d+)/)
+			{
+				$num_fam_inputs = $1;
+				last;
+			}
+		}
+
+
+		# Let's interrogate some stinky ol' exon sets!
+		for (my $fam_input_id=0; $fam_input_id<$num_fam_inputs; $fam_input_id++)
+		{
+
+			my $line = <$SummaryFile>;
+			next if ($line !~ /Input ID/);
+
+			# There was *something* reported -- that's a splicing!
+			$total_num_spliced++;
+
+
+			# For now we'll eat these, but they could
+			# be useful later...
+			$line = <$SummaryFile>; # Runtime
+			$line = <$SummaryFile>; # Model Length
+
+			$line = <$SummaryFile>;
+			$line =~ /Num Exon Sets : (\d+)/;
+			my $num_exon_sets = $1;
+
+
+			# Did this hit get us from 0 to gm->M?
+			my $full_coverage = 0;
+			if ($line =~ /Full model coverage/) {
+				$full_coverage = 1;
+				$num_full_model++;
+			}
+
+
+			# Let's start running through our dang exon sets
+			my $had_missed_exons = 0;
+			for (my $exon_set_id = 1; $exon_set_id <= $num_exon_sets; $exon_set_id++) 
+			{
+
+				$line = <$SummaryFile>; # Exon set ID
+				
+				
+				$line = <$SummaryFile>; # Num. Exons
+				$line =~ /Num Exons\s+: (\d+)/;
+				my $num_exons = $1;
+
+
+				$line = <$SummaryFile>; # Model range
+				$line = <$SummaryFile>; # Nucl. range
+
+
+				$line = <$SummaryFile>; # Missed exons?
+				$had_missed_exons = 1 if ($line =~ /\? Yes/);
+
+
+				for (my $exon_id = 1; $exon_id <= $num_exons; $exon_id++)
+				{
+
+					$line = <$SummaryFile>; # Exon ID
+					$line = <$SummaryFile>; # Model range
+					$line = <$SummaryFile>; # Nucl. range
+
+
+					$line = <$SummaryFile>; # Splice signal
+					$line =~ /: (\S)(\S)\|(\S)(\S)/;
+
+					my $three_one = lc($1);
+					my $three_two = lc($2);
+					my $five_one  = lc($3);
+					my $five_two  = lc($4);
+
+					my $three = $three_one.$three_two;
+					my $five  = $five_one.$five_two;
+
+					if ($three_one ne '-') 
+					{
+						if ($ThreePrimeOne{$three_one}) { $ThreePrimeOne{$three_one}++; }
+						else                            { $ThreePrimeOne{$three_one}=1; }
+					}
+					if ($three_two ne '-') 
+					{
+						if ($ThreePrimeTwo{$three_two}) { $ThreePrimeTwo{$three_two}++; }
+						else                            { $ThreePrimeTwo{$three_two}=1; }
+					}
+					if ($three ne '--')
+					{
+						if ($ThreePrime{$three}) { $ThreePrime{$three}++; }
+						else                     { $ThreePrime{$three}=1; }
+					}
+
+
+					if ($five_one ne '-') 
+					{
+						if ($FivePrimeOne{$five_one}) { $FivePrimeOne{$five_one}++; }
+						else                          { $FivePrimeOne{$five_one}=1; }
+					}
+					if ($five_two ne '-') 
+					{
+						if ($FivePrimeTwo{$five_two}) { $FivePrimeTwo{$five_two}++; }
+						else                          { $FivePrimeTwo{$five_two}=1; }
+					}
+					if ($five ne '--')
+					{
+						if ($FivePrime{$five}) { $FivePrime{$five}++; }
+						else                   { $FivePrime{$five}=1; }
+					}
+
+				}
+
+			}
+
+
+			# Wrap up this input with whether it made use of missed exons
+			if ($had_missed_exons)
+			{
+				$inputs_with_missed++;
+				$full_with_missed++ if ($full_coverage);
+			}
+
+
+		}
+
+
+		close($SummaryFile);
+
+
+	}
+	closedir($AllFamsDir);
+
+
+	# For percentages, we'll want these totals
+	my $total_3prime_sites = 0;
+	foreach my $dinucl (keys %ThreePrime) 
+	{
+		$total_3prime_sites += $ThreePrime{$dinucl};
+	}
+
+	my $total_5prime_sites = 0;
+	foreach my $dinucl (keys %FivePrime)
+	{
+		$total_5prime_sites += $FivePrime{$dinucl};
+	}
+
+
+
+	open(my $FinalResults,'>',$final_results_file_name)
+		|| die "\n  ERROR:  Failed to open final output file '$final_results_file_name'\n\n";
+
+	# First-blush statistics
+	print $FinalResults "Inputs w/ Successful Splicing     : $total_num_spliced\n";
+	print $FinalResults "Inputs w/ Full-Model Splicing     : $num_full_model\n";
+	print $FinalResults "Inputs w/ Hits Using Missed Exons : $inputs_with_missed\n";
+	print $FinalResults "Full-Model Hits w/ Missed Exons   : $full_with_missed\n";
+
+
+	# What did splice signals look like?
+	print $FinalResults "\n";
+	print $FinalResults "3' Splice Signals\n";
+	print $FinalResults "- Dinucleotides\n";
+	foreach my $dinucl (sort keys %ThreePrime)
+	{
+		my $count = $ThreePrime{$dinucl};
+		my $pct   = int(1000.0 * $count / $total_3prime_sites) / 10.0;
+		print $FinalResults "  $dinucl : $count ($pct\%)\n";
+	}
+	print $FinalResults "- [-2] position\n";
+	foreach my $nucl (sort keys %ThreePrimeOne)
+	{
+		my $count = $ThreePrimeOne{$nucl};
+		my $pct   = int(1000.0 * $count / $total_3prime_sites) / 10.0;
+		print $FinalResults "  $nucl  : $count ($pct\%)\n";
+	}
+	print $FinalResults "- [-1] position\n";
+	foreach my $nucl (sort keys %ThreePrimeTwo)
+	{
+		my $count = $ThreePrimeTwo{$nucl};
+		my $pct   = int(1000.0 * $count / $total_3prime_sites) / 10.0;
+		print $FinalResults "   $nucl : $count ($pct\%)\n";
+	}
+
+	print $FinalResults "\n";
+	print $FinalResults "5' Splice Signals\n";
+	print $FinalResults "- Dinucleotides\n";
+	foreach my $dinucl (sort keys %FivePrime)
+	{
+		my $count = $FivePrime{$dinucl};
+		my $pct   = int(1000.0 * $count / $total_5prime_sites) / 10.0;
+		print $FinalResults "  $dinucl : $count ($pct\%)\n";
+	}
+	print $FinalResults "- [+1] position\n";
+	foreach my $nucl (sort keys %FivePrimeOne)
+	{
+		my $count = $FivePrimeOne{$nucl};
+		my $pct   = int(1000.0 * $count / $total_5prime_sites) / 10.0;
+		print $FinalResults "  $nucl  : $count ($pct\%)\n";
+	}
+	print $FinalResults "- [+2] position\n";
+	foreach my $nucl (sort keys %FivePrimeTwo)
+	{
+		my $count = $FivePrimeTwo{$nucl};
+		my $pct   = int(1000.0 * $count / $total_5prime_sites) / 10.0;
+		print $FinalResults "   $nucl : $count ($pct\%)\n";
+	}
+	print $FinalResults "\n";
+
+	close($FinalResults);
+
+}
 
 
 
@@ -158,29 +432,215 @@ sub CompileBasicResults
 		# top for each query
 		my $num_exon_sets = 0;
 		my $has_full_hit  = 0;
+		my $chromosome;
+		my $chr_start;
 		while (my $line = <$SplOutFile>) 
 		{
+			
 			$num_exon_sets++  if ($line =~ /\| = Exon Set/);
 			$has_full_hit = 1 if ($line =~ /\(\* Full Model\)/);
-		}
-		print $SummaryFile "  Num Exon Sets : $num_exon_sets\n";
 
-		# We can jump the gun *just a bit* in this case:
+			# This should capture a nucleotide sequence row
+			if (!$chromosome && $line =~ /^\s+(\S+)\/(\d+)-\d+\s+\d+/)
+			{
+				$chromosome = $1;
+				$chr_start  = $2;
+			}
+
+		}
+		print $SummaryFile "  Num Exon Sets : $num_exon_sets";
+
+
+		# We can show our excitement *just a bit* in this case:
 		if ($has_full_hit && $num_exon_sets == 1)
 		{
-			print $SummaryFile "                * Full model coverage!\n";
+			print $SummaryFile " (Full model coverage!)";
 		}
+		print $SummaryFile "\n";
 
 
 		# Close and reopen the file :p
 		close($SplOutFile);
 		open($SplOutFile,'<',$spl_out_file_name)
 			|| die "\n  ERROR:  Failed to open output file '$spl_out_file_name' (BAD NEWS!)\n\n";
+		for (my $exon_set_id = 1; $exon_set_id <= $num_exon_sets; $exon_set_id++) 
+		{
 
+			print $SummaryFile "  - Exon Set    : $exon_set_id\n";
+
+
+			# Advance to the start of the next exon set
+			my $num_exons;
+			while (my $line = <$SplOutFile>)
+			{
+				if ($line =~ /= Exon Set \d+ \((\d+) exons\)/) 
+				{
+					$num_exons = $1;
+					last;
+				}
+			}
+			print $SummaryFile "  - Num Exons   : $num_exons\n";
+
+
+			# What range of model / nucleotide positions are covered?
+			# NOTE: We'll need to correct these!
+			my $model_pos_line = <$SplOutFile>;
+			$model_pos_line =~ /(\d+)\.\.(\d+)/;
+			my $model_start = $1;
+			my $model_end   = $2;
+
+			print $SummaryFile "  - Model Range : $model_start..$model_end\n";
+
+
+			my $nucl_coord_line = <$SplOutFile>;
+			$nucl_coord_line =~ /(\d+)\.\.(\d+)/;
+			my $nucl_start = $1 + ($chr_start-1);
+			my $nucl_end   = $2 + ($chr_start-1);
+
+			# We *always* need this.
+			my $revcomp = 0;
+			$revcomp = 1 if ($nucl_start > $nucl_end);
+
+			# The nucleotide coordinates will include the splice signals,
+			# which is nice, but not really part of the hit, per se.
+			if ($revcomp) {
+				$nucl_start -= 2;
+				$nucl_end   += 2;
+			} else {
+				$nucl_start += 2;
+				$nucl_end   -= 2;
+			}
+			print $SummaryFile "  - Nucl. Range : $nucl_start..$nucl_end\n";
+
+
+			# Did this hit incorporate "missed" exons?
+			my $has_missed_exons  = 0;
+			while (my $missed_exons_line = <$SplOutFile>) 
+			{
+				last if ($missed_exons_line !~ /^\|/);
+
+				if ($missed_exons_line =~ /\| \+ Includes Missed Exons/)
+				{
+					$has_missed_exons = 1;
+					last;
+				}
+			}
+
+			print $SummaryFile "  - Missed Exons? ";
+			if ($has_missed_exons) { print $SummaryFile "Yes\n"; }
+			else                   { print $SummaryFile "No\n";  }
+
+
+			# Now we can iterate over the exons that make up this
+			# "exon set" and report their particular datums!
+			my $exon_id = 0;
+			my $exon_model_start = 0;
+			my $exon_model_end;
+			my $exon_nucl_start = 0;
+			my $exon_nucl_end;
+			my $exon_nucl_seq = ''; # Stupid lazy way to get the splice dinucleotides...
+			while ($exon_id <= $num_exons && !eof($SplOutFile))
+			{
+
+				my $line = <$SplOutFile>;
+
+				$line =~ s/\n|\r//g;
+				next if (!$line);
+
+
+				# Are we starting off a new exon? Are we at *the end*?!
+				if ($line =~ /\[ Exon Set/ || $line =~ /^\+=========/)
+				{
+
+					# Don't sweat it, special baby zero
+					if ($exon_id == 0)
+					{
+						$exon_id = 1;
+						next;
+					}
+
+
+					# We need to account for the fact that the splice signal
+					# eats two of our precious nucleotides
+					if ($revcomp) {
+						$exon_nucl_start -= 2;
+						$exon_nucl_end   += 2;
+					} else {
+						$exon_nucl_start += 2;
+						$exon_nucl_end   -= 2;
+					}
+
+					
+					# What's the splice signal, Kenneth?
+					my $exon_3prime = '--';
+					if ($exon_id > 1)
+					{
+						$exon_nucl_seq =~ /^(\S\S)/;
+						$exon_3prime = $1;
+					}
+
+					my $exon_5prime = '--';
+					if ($exon_id < $num_exons)
+					{
+						$exon_nucl_seq =~ /(\S\S)$/;
+						$exon_5prime = $1;
+					}
+
+
+					print $SummaryFile "    + Exon $exon_id\n";
+					print $SummaryFile "      Model Range   : $exon_model_start..$exon_model_end\n";
+					print $SummaryFile "      Nucl. Range   : $exon_nucl_start..$exon_nucl_end\n";
+					print $SummaryFile "      Splice Signal : $exon_3prime|$exon_5prime\n";
+
+					$exon_model_start = 0;
+					$exon_nucl_start  = 0;
+					$exon_nucl_seq    = '';
+
+					$exon_id++;
+					next;
+
+				}
+
+
+				# Are we hitting a *chill* line?
+				if ($exon_id && $line =~ /^\s+\S+\s+(\d+).*\s+(\d+)\s*$/)
+				{
+					
+					my $ali_line_model_start = $1;
+					my $ali_line_model_end   = $2;
+
+					$exon_model_start = $ali_line_model_start if (!$exon_model_start);
+					$exon_model_end   = $ali_line_model_end;
+
+
+					$line = <$SplOutFile>; # Quality line
+					$line = <$SplOutFile>; # Translation line
+					$line = <$SplOutFile>; # Nucleotides!
+
+
+					$line =~ /^\s+\S+\s+(\d+)\s+(\S+)\s+(\d+)\s*$/;
+					my $ali_line_nucl_start = $1;
+					my $ali_line_nucl_seq   = $2;
+					my $ali_line_nucl_end   = $3;
+
+					# NOTE: We'll deal with the add/sub-2 for splice signal *later*
+					$exon_nucl_start = $ali_line_nucl_start + ($chr_start-1) if (!$exon_nucl_start);
+					$exon_nucl_end   = $ali_line_nucl_end   + ($chr_start-1);
+					$line = <$SplOutFile>; # PP line (I'm a child)
+
+					$exon_nucl_seq = $exon_nucl_seq.$ali_line_nucl_seq;
+
+				}
+
+			}
+
+		}
 
 		close($SplOutFile);
 
 	}
+
+	close($SummaryFile);
 
 }
 
@@ -429,7 +889,8 @@ sub BigBadSplash
 		my $family = $1;
 
 		# NOTE: The 'successes' and 'errors' are the base names of the files
-		#       (e.g., 'file.out' minus '.out')
+		#       (e.g., 'file.out' minus '.out').  Currently, we aren't really
+		#       using these.  But we could.  And that's what matters.
 		#
 		my ($fam_out_dir_name,$fam_successes_ref,$fam_errors_ref) 
 			= FamilySplash($fam_in_dir_name);
@@ -458,6 +919,12 @@ sub BigBadSplash
 			system("rm \"$thread_err_file\"");
 		}
 	}
+
+
+	# Now that the ugliness is over, the FUN!!!
+	# Combine all of our family-specific output data into
+	# one nice big output file.
+	AggregateAllResults($rbg_dir_name);
 
 
 }
