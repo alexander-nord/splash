e0f39cadf2c33f002db74e73e490d391a28b78bf
alexander-nord <alexnord925@gmail.com>
2024-05-30 05:45:22 -0600

    Changed to compute 1,2 / 3+ rather than just non-0

--- inc/analysis-scripts/dist-to-known-juncts/ZeroVsNonZero.pl
+++ inc/analysis-scripts/dist-to-known-juncts/ZeroVsNonZero.pl
@@ -10,8 +10,9 @@ if (@ARGV != 1) { die "\n  USAGE:  ./ZeroVsNonZero.pl [junction-nucl-diff-counts
 open(my $CSV,'<',$ARGV[0])
 	|| die "\n  ERROR:  Failed to open input file '$ARGV[0]'\n\n";
 
-my    $zero = 0;
-my $nonzero = 0; # lol
+my $zero       = 0;
+my $one_or_two = 0;
+my $three_plus = 0;
 
 while (my $line = <$CSV>)
 {
@@ -20,25 +21,33 @@ while (my $line = <$CSV>)
 		my $dist  = $1;
 		my $count = $2;
 
-		if ($dist == 0) {    $zero  = $count; }
-		else            { $nonzero += $count; }
+		if    ($dist == 0) {    $zero     = $count; }
+		elsif ($dist == 1) { $one_or_two += $count; }
+		elsif ($dist == 2) { $one_or_two += $count; }
+		else               { $three_plus += $count; }
 	}
 }
 
 close($CSV);
 
 
-my $zero_pct = int(1000.0 * $zero / ($zero + $nonzero)) / 10.0;
+my $num_juncts = $zero + $one_or_two + $three_plus;
 
 
-# Am I dumb or what?
-$zero = ' '.$zero while (length("$zero") < length("$nonzero"));
-$nonzero = ' '.$nonzero while (length("$nonzero") < length("$zero"));
+my $zero_pct       = int(10000.0 * $zero       / $num_juncts) / 100.0;
+my $one_or_two_pct = int(10000.0 * $one_or_two / $num_juncts) / 100.0;
+my $three_plus_pct = int(10000.0 * $three_plus / $num_juncts) / 100.0;
+
+
+# Assuming 0 is the majority
+$one_or_two = ' '.$one_or_two while (length("$one_or_two") < length("$zero"));
+$three_plus = ' '.$three_plus while (length("$three_plus") < length("$zero"));
 
 
 print "\n";
-print " Zero := $zero  ==>  ($zero_pct\%)\n";
-print "!Zero := $nonzero\n";
+print "  Zero := $zero => $zero_pct\%\n";
+print "  1,2  := $one_or_two => $one_or_two_pct\%\n";
+print "  3+   := $three_plus =>  $three_plus_pct\%\n";
 print "\n";
 
 
