93f0fa4f64831f5ad6684714d14bb0a6ae4acbdd
alexander-nord <alexnord925@gmail.com>
2024-06-18 08:42:36 -0600

    Added script for comparatively running Splash and MiniProt

---
+++ inc/slurm-scripts/Splash-vs-MP-PANTHER-mammals.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=spl-vs-mp
+#SBATCH --array=1-20
+#SBATCH --time=50:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30G
+#SBATCH --partition=standard
+#SBATCH --account=twheeler
+
+#
+#  NOTE:  BE SURE TO INITIALIZE YOUR OUTPUT DIRECTORIES AHEAD OF TIME!!!!
+#         ^ This includes 'Results-by-Gene'!
+#
+
+SPECIES_GUIDE="$HOME/data/PANTHER-species-guide"
+IN_DIR_NAME="/xdisk/twheeler/alexnord/mammal-PANTHER"
+OUT_DIR_NAME="/xdisk/twheeler/alexnord/results/Splash-vs-MP-Output"
+
+cd $HOME/splash
+
+time perl Run-Splash-Test.pl --slurm $SLURM_ARRAY_TASK_ID --miniprot --panther -o $OUT_DIR_NAME -n $SLURM_ARRAY_TASK_COUNT $IN_DIR_NAME $SPECIES_GUIDE 1>$SLURM_ARRAY_TASK_ID.fresh.out 2>$SLURM_ARRAY_TASK_ID.fresh.err
+
